fd70fbe1803f0a99292dec77cb18b8224f416595
FrankFan <fanyong@gmail.com>
2015-08-15 14:02:33 +0800

    use jshint to check js syntax error

---
+++ jshint/clean.js
@@ -0,0 +1,9 @@
+/*
+* 首先需要安装jshint的npm包
+* $ sudo npm install -g jshint
+* $ jshint clean.js
+*/
+
+function messy_javascript() {
+	console.log('This is a line with ' + 'good' + ' concatenation.');
+}
