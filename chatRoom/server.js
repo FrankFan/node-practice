572fed9843bb0b6f6d64616d9358c6395fa565de
FrankFan <fanyong@gmail.com>
2015-08-16 11:45:19 +0800

    listen exit event

--- chatRoom/server.js
+++ chatRoom/server.js
@@ -4,4 +4,14 @@ var io = require('socket.io'),
 var app = connect()
 	.use(connect.static('public'))
 	.listen(3000);
-var socket = io.listen(app);
+var chat_room = io.listen(app);
+
+chat_room.sockets.on('connection', function(socket) {
+	socket.emit('entrance', {message: 'Welcome to the chat room!'});
+
+	socket.on('disconnect', function() {
+		chat_room.sockets.emit('exit', {message: 'A chatter has disconnect.'});
+	});
+
+	chat_room.sockets.emit('entrance', {message: 'A new chatter is online.'});	
+});
