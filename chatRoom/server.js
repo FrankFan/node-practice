5607c2aec2f15ff155554a3b4aee188bed6352cf
FrankFan <fanyong@gmail.com>
2015-09-05 23:53:44 +0800

    done

--- chatRoom/server.js
+++ chatRoom/server.js
@@ -7,16 +7,11 @@ var app = connect()
 	.listen(3000);
 var chat_room = io.listen(app);
 
+chatter.set_sockets(chat_room.sockets);
+
 chat_room.sockets.on('connection', function(socket) {
-	socket.emit('entrance', {message: 'Welcome to the chat room!'});
-
-	socket.on('disconnect', function() {
-		chat_room.sockets.emit('exit', {message: 'A chatter has disconnect.'});
+	chatter.connect_chatter({
+		socket: socket,
+		username: socket.id
 	});
-
-	socket.on('chat', function(data) {
-		chat_room.sockets.emit('chat', {message: '# ' + data.message});
-	});
-
-	chat_room.sockets.emit('entrance', {message: 'A new chatter is online.'});	
 });
