dda8d1f1cc54b4c1ade884839fe719ae76688d2b
fanyong <fanyong557@lu.com>
2017-10-09 20:09:59 +0800

    node beginners tutorial

---
+++ node-app-upload/README.md
@@ -0,0 +1,3 @@
+
+
+https://www.nodebeginner.org/index-zh-cn.html#a-full-blown-web-application-with-nodejs
---
+++ node-app-upload/server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const url = require('url');
+
+function start(route, handle) {
+  function onRequest(request, response) {
+    const pathname = url.parse(request.url).pathname;
+    console.log('Request for ' + pathname + ' revieved.');
+
+    response.writeHead(200, {
+      'Content-Type': 'text/plain'
+    });
+
+    const content = route(handle, pathname);
+
+    response.write(content);
+    response.end();
+  }
+
+  http.createServer(onRequest).listen(9000);
+  console.log('Server has started.');
+}
+
+exports.start = start;
